736ceda05d0b9c3fbcaaec0744b72637ad5ff00f
zaru <zarutofu@gmail.com>
2026-01-24 19:42:48 +0900

    コメントを追加

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,22 @@
 module Main where
 
+-- ライブラリのインポート時に関数を指定することができる
+-- 指定しないとグローバルにすべて展開される
+-- import qualified Foo as F と別名をつけることもできる
 import Control.Monad (forever)
-import Network.Socket
+import Network.Socket (socket, bind, listen, accept)
 
 main :: IO ()
 main = do
     sock <- serveSocket 3000
+    -- listenは指定ソケットで待機接続数を指定
     listen sock 5
+    -- memo: $は右側の式をすべて評価してから、左側の関数に渡す、カッコ () の代わり
+    -- forever は無限ループする
     forever $ do
         (conn, addr) <- accept sock
+        -- ++は文字列リストの結合、文字列は文字のリストなのでリスト同士を結合する
+        -- showは .toString() 相当のもの、addr 変数を文字列に変換する
         putStrLn $ "接続: " ++ show addr
         close conn
 

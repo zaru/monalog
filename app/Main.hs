3960a643b820f33199d09a5ff910214a68e1f918
zaru <zarutofu@gmail.com>
2026-01-24 17:24:23 +0900

    Socketでポートをバインドする実装

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,19 @@
 module Main where
 
+import Control.Monad (forever)
+import Network.Socket
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    sock <- serveSocket 3000
+    listen sock 5
+    forever $ do
+        (conn, addr) <- accept sock
+        putStrLn $ "接続: " ++ show addr
+        close conn
+
+serveSocket :: PortNumber -> IO Socket
+serveSocket port = do
+    sock <- socket AF_INET Stream defaultProtocol
+    bind sock (SockAddrInet port (tupleToHostAddress (0,0,0,0)))
+    return sock

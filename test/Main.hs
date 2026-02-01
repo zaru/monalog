3f319b5e6ff8b1ae0ed45361f7e3813820bc8fce
zaru <zarutofu@gmail.com>
2026-02-01 16:05:56 +0900

    Markdown旧実装を削除

--- test/Main.hs
+++ test/Main.hs
@@ -1,6 +1,5 @@
 import Monalog.Markdown.ParserTest qualified
 import Monalog.Markdown.RenderTest qualified
-import Monalog.MarkdownTest qualified
 import Test.Tasty
 
 main :: IO ()
@@ -8,7 +7,6 @@ main =
   defaultMain $
     testGroup
       "All Tests"
-      [ Monalog.MarkdownTest.tests,
-        Monalog.Markdown.ParserTest.tests,
+      [ Monalog.Markdown.ParserTest.tests,
         Monalog.Markdown.RenderTest.tests
       ]

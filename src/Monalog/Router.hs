d4f7379f54f07dfda6b935d9f5dc2823640b8cb8
zaru <zarutofu@gmail.com>
2026-02-03 20:21:34 +0900

    Routerをリファクタリング

--- src/Monalog/Router.hs
+++ src/Monalog/Router.hs
@@ -1,40 +1,72 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Monalog.Router (parseRequest, Request (..), Method (..)) where
+module Monalog.Router (parseRequest, HttpRequest (..), HttpMethod (..)) where
 
 import Data.ByteString (ByteString)
-import Data.ByteString.Char8 qualified as BC
+import Data.ByteString.Char8 qualified as B -- ByteStringを扱いやすくするためにChar8にする
 
-data Method = Get deriving (Show, Eq)
+data HttpMethod = GET | POST | PUT | DELETE | PATCH | HEAD | OPTIONS
+  deriving (Show, Eq)
 
-data Request = NewRequest
-  { method :: Method,
-    path :: ByteString
+data HttpRequest = HttpRequest
+  { method :: HttpMethod,
+    path :: ByteString,
+    version :: ByteString,
+    headers :: [(ByteString, ByteString)]
   }
   deriving (Show, Eq)
 
-parseRequest :: ByteString -> Request
-parseRequest line =
-  let (m, p) = parseMethod line
-  in NewRequest m p
+-- パースを失敗する可能性があるのでMaybeにする
+parseRequest :: ByteString -> Maybe HttpRequest
+-- do式を使うことでMaybeのNothing対応を省略できる
+-- 途中でNothingが発生したら、その時点で処理が終了
+-- 使わない場合はcase..of Nothingの条件分岐が必要
+parseRequest raw = do
+  (reqLine, rest) <- splitLine raw
+  (m, p, v) <- parseRequestLine reqLine
+  pure
+    HttpRequest
+      { method = m,
+        path = p,
+        version = v,
+        headers = parseHeaders rest
+      }
 
-parseMethod :: ByteString -> (Method, ByteString)
-parseMethod line =
-  case BC.break (== ' ') line of
-    (method, rest)
-      | method == "GET" -> (Get, parsePath cleanRest)
-      | otherwise -> (Get, parsePath cleanRest)
-      where
-        cleanRest = BC.dropWhile (== ' ') rest
+parseRequestLine :: ByteString -> Maybe (HttpMethod, ByteString, ByteString)
+parseRequestLine line = case B.words line of
+  -- Applicative Style : Maybeの失敗するかも知れない文脈の中で関数適用させる書き方
+  -- (,,) 3要素のタプルコンストラクタ関数
+  -- <$> fmapのこと。リストに使えるmapをなんにでも使えるようにするもの、今回はMaybeな関数に適用している
+  -- <*> applyのこと。MaybeとMaybeをくっつける
+  -- pure Justと同じ。Maybeの生の値を抜き出す
+  [m, p, v] -> (,,) <$> parseMethod m <*> pure p <*> pure v
+  _ -> Nothing
 
-parsePath :: ByteString -> ByteString
--- 合成を使って書くことができる
--- parsePath line = fst $ BC.break (== '?') $ fst $ BC.break (== ' ') line
-parsePath = fst . BC.break (== '?') . fst . BC.break (== ' ')
+parseMethod :: ByteString -> Maybe HttpMethod
+parseMethod "GET" = Just GET
+parseMethod "POST" = Just POST
+parseMethod "PUT" = Just PUT
+parseMethod "DELETE" = Just DELETE
+parseMethod "PATCH" = Just PATCH
+parseMethod "HEAD" = Just HEAD
+parseMethod "OPTIONS" = Just OPTIONS
+parseMethod _ = Nothing
 
-request :: Method -> Request
-request Get =
-  NewRequest
-    { method = Get,
-      path = "/"
-    }
+parseHeaders :: ByteString -> [(ByteString, ByteString)]
+parseHeaders bs = case splitLine bs of
+  Nothing -> []
+  Just ("", _) -> [] -- 空行 = ヘッダー終了
+  Just (line, rest) -> case B.break (== ':') line of
+    (key, val)
+      | not (B.null val) ->
+          -- Valueの先頭の : を削除したうえで、スペースを全部削除する
+          -- 残り行数を再帰処理をする
+          (key, B.dropWhile (== ' ') (B.tail val)) : parseHeaders rest
+    _ -> parseHeaders rest
+
+splitLine :: ByteString -> Maybe (ByteString, ByteString)
+splitLine bs = case B.breakSubstring "\r\n" bs of
+  (line, rest)
+    | B.null rest -> Nothing
+    -- 分割で先頭に残っている \r\n を drop して削除
+    | otherwise -> Just (line, B.drop 2 rest)
--- test/Monalog/RouterTest.hs
+++ test/Monalog/RouterTest.hs
@@ -12,15 +12,24 @@ tests =
   testGroup
     "Router parseRequest"
     [ testCase "Top Page" $
-        parseRequest (encodeUtf8 "GET / HTTP")
-          @?= NewRequest
-            { method = Get,
-              path = "/"
-            },
+        parseRequest (encodeUtf8 "GET / HTTP/1.1\r\n")
+          @?= Just
+            HttpRequest
+              { method = GET,
+                path = encodeUtf8 "/",
+                version = encodeUtf8 "HTTP/1.1",
+                headers = []
+              },
       testCase "Article Page" $
-        parseRequest (encodeUtf8 "GET /s/202601011223 HTTP")
-          @?= NewRequest
-            { method = Get,
-              path = "/s/202601011223"
-            }
+        parseRequest (encodeUtf8 "GET /s/202601011223 HTTP/1.1\r\n")
+          @?= Just
+            HttpRequest
+              { method = GET,
+                path = encodeUtf8 "/s/202601011223",
+                version = encodeUtf8 "HTTP/1.1",
+                headers = []
+              },
+      testCase "Invalid" $
+        parseRequest (encodeUtf8 "INVALID")
+          @?= Nothing
     ]
